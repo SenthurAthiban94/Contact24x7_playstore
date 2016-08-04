38b3594118757dfc09fe385bc5ecf5087c2a1919
SenthurAthiban94 <senthurathiban94@gmail.com>
2016-08-04 11:37:32 +0530

    Contact24x7 is back

--- js/horoscope.js
+++
@@ -1,6 +0,0 @@
-var appname=angular.module('Horoscope_Predictor',[]);
-appname.controller('horoscopecontroller',['$scope',function($scope){
-	$scope.change=function(){
-		$scope.contents=false;
-	}
-}]);
